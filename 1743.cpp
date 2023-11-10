f74acad0f0dd6a42f29f73c617f1f13e325863f5
maxioul <maxiou7391468250@126.com>
2023-11-10 09:16:07 +0900

    Create 1743.cpp

---
+++ 1743.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/description/?envType=daily-question&envId=2023-11-10
+
+//Find one of the starting point.
+
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+    unordered_map<int, vector<int>> ps;
+    for (auto &p : adjacentPairs) {
+        ps[p[0]].push_back(p[1]);
+        ps[p[1]].push_back(p[0]);
+    }
+    vector<int> res;
+    for (auto &p : ps) {
+        if (p.second.size() == 1) {
+            res.push_back(p.first);
+            res.push_back(p.second[0]);
+            break;
+        }
+    }
+    while (res.size() < adjacentPairs.size() + 1) {
+        auto tail = res.back(), prev = res[res.size() - 2];
+        auto &next = ps[tail];
+        if (next[0] != prev)
+            res.push_back(next[0]);
+        else
+            res.push_back(next[1]);
+    }
+    return res;
+}
+};
